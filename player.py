f4db25b34d1348a5ad4c44969953ffaf58b5a9c6
iambb5445 <bbateni@ucsc.edu>
2025-05-06 12:31:00 -0700

    Add heuristic to random players, refactor players

--- player.py
+++ player.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
 from abc import ABC, abstractmethod
 from game import Game, GameAction
 import random
 from parser import Parser
+from typing import Callable
 
 class Player(ABC):
     # This function returns an str instead of a GameAction,
@@ -12,31 +14,44 @@ class Player(ABC):
         raise NotImplementedError
     
 class RandomPlayer(Player):
-    def __init__(self, seed: int|None = None) -> None:
+    def __init__(self, seed: int|None = None, heuristic: Callable[[Game], int]|None=None) -> None:
         self.random = random.Random(seed)
+        self.heuristic = heuristic
+
+    def _get_performed_state(self, current_state: Game, action: GameAction) -> Game:
+        new_state = current_state.copy()
+        new_state.logger.temp_deactivate()
+        Parser.perform_action_in_game(str(action), new_state)
+        return new_state
+
+    def _weighted_choice(self, current_state: Game, actions: list[GameAction]) -> GameAction|None:
+        if len(actions) == 0:
+            return None
+        if self.heuristic is None:
+            return self.random.choice(actions)
+        values = [(self.heuristic(self._get_performed_state(current_state, action)), action) for action in actions]
+        return max(values)[1] # TODO weighted random
 
     def decide_action(self, current_state: Game) -> str|None:
         actions = current_state.get_possible_actions(True)
-        if len(actions) == 0:
-            return None
-        action = self.random.choice(actions)
-        return str(action)
+        action = self._weighted_choice(current_state, actions)
+        return str(action) if action is not None else None
 
 class RandomNoRepeatPlayer(RandomPlayer):
-    def __init__(self, seed:int|None = None) -> None:
-        super().__init__(seed)
-        self.seen_states = set()
+    HASH_TYPE = str # TODO generic
+    def __init__(self, seed:int|None = None, heuristic: Callable[[Game], int]|None = None) -> None:
+        super().__init__(seed, heuristic)
+        self.seen_states: set[RandomNoRepeatPlayer.HASH_TYPE] = set()
 
-    def _hash(self, s: str) -> int:
-        return len(s)
+    def _hash(self, state: Game) -> HASH_TYPE:
+        return state.get_game_view()
+    
+    def _get_new_state_hash(self, current_state: Game, action: GameAction) -> HASH_TYPE:
+        return self._hash(self._get_performed_state(current_state, action))
     
     def decide_action(self, current_state: Game) -> str|None:
+        self.seen_states.add(self._hash(current_state))
         actions = current_state.get_possible_actions(True)
-        def filter(action: GameAction):
-            state_copy = current_state.copy()
-            Parser.perform_action_in_game(str(action), state_copy)
-            return self._hash(state_copy.get_game_view()) not in self.seen_states
-        actions = [action for action in actions if filter(action)]
-        if len(actions) == 0:
-            return None
-        return str(self.random.choice(actions))
+        actions = [action for action in actions if self._get_new_state_hash(current_state, action) not in self.seen_states]
+        action = self._weighted_choice(current_state, actions)
+        return str(action) if action is not None else None
