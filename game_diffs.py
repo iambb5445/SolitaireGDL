94a256f8647e64802712280a84fc99d6e394ef7d
iambb5445 <bbateni@ucsc.edu>
2025-05-28 10:44:38 -0700

    Create a table showing diffs between games

--- game_diffs.py
+++ game_diffs.py
@@ -1,10 +1,20 @@
 from parser import Parser
 from diffs import Diffs
+import pandas as pd
 
 if __name__ == '__main__':
-    game_filenames = ['games\\klondike.sgdl', 'games\\spider.sgdl']
+    game_filenames = ['games\\klondike.sgdl', 'games\\spider.sgdl']#, 'games\\easiestspider.sgdl']
     games = [Parser.from_file(filename, None, False, True) for filename in game_filenames]
-    for game1 in games:
-        for game2 in games:
-            diff = game1.diff(game2, True, Diffs.get_sum_normalized_diff_normalized)
-            print(f'Diff between {game1.name} and {game2.name} is {diff.get_sum_normalized_diff_normalized()}, {diff.get_normalized_diff_points()}')
+    diff_vals: list[list[float]] = [[0 for _ in games] for _ in games]
+    diff_points: list[list[float]] = [[0 for _ in games] for _ in games]
+    for i in range(len(games)):
+        for j in range(len(games)):
+            diff = games[i].diff(games[j], True, Diffs.get_sum_normalized_diff_normalized)
+            diff_vals[i][j] = diff.get_sum_normalized_diff_normalized()
+            diff_points[i][j] = diff.get_normalized_diff_points()
+    names: list[str] = [game.name for game in games]
+    print('-' * 25)
+    print(pd.DataFrame(diff_vals, columns=names, index=names))
+    print('-' * 25)
+    print(pd.DataFrame(diff_points, columns=names, index=names))
+    print('-' * 25)
