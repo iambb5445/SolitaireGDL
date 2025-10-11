d290a445a53d7fd06406ce3ace0700ac5ace4877
iambb5445 <bbateni@ucsc.edu>
2025-10-10 23:17:06 -0700

    Add argparse for run_cmd

--- run_cmd.py
+++ run_cmd.py
@@ -2,15 +2,22 @@ from parser import Parser
 from utility import Logger
 import utility as util
 import sys
+import argparse
 
 if __name__ == '__main__':
-    sgdl_filename = sys.argv[1]
-    game = Parser.from_file(sgdl_filename, 0, True, True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str, help="Name of the SGDL file defining game rules. Refer to games/ for examples.")
+    parser.add_argument('seed', type=int, nargs="?", default=0, help="Integer seed to be used for shuffling the deck.")
+    parser.add_argument('--partial-info', action="store_true", help="Show only the cards that are faced up. Face down cards will be shown as [?]")
+    args = parser.parse_args(sys.argv[1:])
+    sgdl_filename: str = args.filename
+    seed = args.seed
+    game = Parser.from_file(sgdl_filename, seed, True, True)
     logger = Logger(True)
     logger.info("GAME START!")
     while not game.is_win():
-        logger.info(game.get_state_view())
-        logger.info(f"{len(game.get_possible_actions(False))} actions")
+        logger.info(game.get_game_view() if args.partial_info else game.get_state_view())
+        logger.info(f"{len(game.get_possible_actions(False))} total actions")
         valid_actions = game.get_possible_actions(True)
         logger.info(f"{len(valid_actions)} valid actions:")
         for i, valid_action in enumerate(valid_actions):
