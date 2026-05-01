3022dcea6dd1de33c84efad0cc5fb94b934b6649
iambb5445 <bbateni@ucsc.edu>
2026-05-01 09:02:50 -0700

    Add step when evaluating dir

--- evaluate_dir.py
+++ evaluate_dir.py
@@ -4,7 +4,12 @@ from parser import Parser
 from genetic import evaluate_gdl, Verdict
 
 filepath = sys.argv[1]
+step = int(sys.argv[2])
+
+print(f"Evaluating games in {filepath} with step {step}")
+
 filenames = [name for name in os.listdir(filepath) if name.split('.')[-1] == 'sgdl']
+filenames = [name for id, name in enumerate(filenames) if id % step == 0]
 gdls: list[str] = []
 for filename in filenames:
     with open(os.path.join(filepath, filename), 'r') as f:
