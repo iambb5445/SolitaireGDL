98fa326bff49e474ddb2a18b6e8805d8cd5fc48b
iambb5445 <bbateni@ucsc.edu>
2026-07-01 07:52:43 -0700

    Add retries to crossover for failsafe

--- job_scripts/generate_crossover.py
+++ job_scripts/generate_crossover.py
@@ -11,6 +11,33 @@ from utility import get_seed
 from history import History
 
 seed_max = 1000000000
+max_retries = 50
+
+def generate_crossover(ind: int, parent_filenames):
+    gene1 = GeneParser.from_file(os.path.join(dir, parent_filenames[0]))
+    gene2 = GeneParser.from_file(os.path.join(dir, parent_filenames[1]))
+    for attempt in range(max_retries):
+        seed = get_seed(experiment_rnd, seed_max)
+        try:
+            new_gene = gene1.crossover(gene2, Random(seed), False)
+            gdl = new_gene.get_gdl()
+            # validation
+            Parser.parse(gdl, None, False, True)
+            name = Parser.get_name(gdl)
+            hash = new_gene.get_hash()
+            main_parent_hash = gene1.get_hash()
+            secondary_parent_hash = gene2.get_hash()
+            history.add(
+                timestamp, experiment_seed, seed, name, hash, History.GEN_METHOD.CROSSOVER,
+                main_parent_hash, secondary_parent_hash, prev_history.get_ancestor(main_parent_hash))
+            with open(os.path.join(outpath, f"{ind}_{name}_{hash}.sgdl"), "w") as f:
+                f.write(gdl)
+            return True
+        except Exception as e:
+            if not args.ignore_errors:
+                raise e
+            print(f"!!![ERROR] (attempt {attempt+1}/{max_retries}, seed: {seed},  parents: {parent_filenames}): {e}")
+    return False
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -43,31 +70,13 @@ if __name__ == "__main__":
         exit()
     game_usage: dict[str, int] = dict([(file, 0) for file in filenames])
     for i in range(args.count):
-        try:
-            ind = i + base_index
-            if len(filenames) < 2:
-                break
-            parent_filenames = experiment_rnd.sample(filenames, k=2)
+        ind = i + base_index
+        if len(filenames) < 2:
+            break
+        parent_filenames = experiment_rnd.sample(filenames, k=2)
+        if generate_crossover(ind, parent_filenames):
             for parent_filename in parent_filenames:
                 game_usage[parent_filename] += 1
                 if max_per_game is not None and game_usage[parent_filename] >= max_per_game:
                     filenames.remove(parent_filename)
-            gene1 = GeneParser.from_file(os.path.join(dir, parent_filenames[0]))
-            gene2 = GeneParser.from_file(os.path.join(dir, parent_filenames[1]))
-            seed = get_seed(experiment_rnd, seed_max)
-            new_gene = gene1.crossover(gene2, Random(seed), False)
-            gdl = new_gene.get_gdl()
-            name = Parser.get_name(gdl)
-            hash = new_gene.get_hash()
-            main_parent_hash = gene1.get_hash()
-            secondary_parent_hash = gene2.get_hash()
-            history.add(
-                timestamp, experiment_seed, seed, name, hash, History.GEN_METHOD.CROSSOVER,
-                main_parent_hash, secondary_parent_hash, prev_history.get_ancestor(main_parent_hash))
-            with open(os.path.join(outpath, f"{ind}_{name}_{hash}.sgdl"), "w") as f:
-                f.write(gdl)
-        except Exception as e:
-            if not args.ignore_errors:
-                raise e
-            print(f"!!![ERROR]: {e}")
     history.to_csv(os.path.join(outpath, f"history.csv"), True)
